a6209cee078a536743cf80ce6a8f9f5596896540
Flily Hsu <1126143+flily@users.noreply.github.com>
2026-07-01 02:28:53 +0800

    add 200ms extra timeout for p60

--- src/problems/p0060/mod.rs
+++ src/problems/p0060/mod.rs
@@ -6,6 +6,7 @@ mod optimized;
 pub static INFO: std::sync::LazyLock<Problem> = std::sync::LazyLock::new(||
     Problem::init(60, "Prime Pair Sets")
         .with_answer(26033)
+        .extra_timeout_ms(200)
         .solution("naive", naive::solve)
         .solution("search pair list", optimized::solve)
 );
