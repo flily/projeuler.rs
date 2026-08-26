a46c54d84508ccc0c80e637726c1197dd6d1ded0
Flily Hsu <1126143+flily@users.noreply.github.com>
2026-08-27 02:00:39 +0800

    update solution of p81

--- src/problems/p0081/mod.rs
+++ src/problems/p0081/mod.rs
@@ -2,12 +2,14 @@ use crate::framework::Problem;
 
 mod bruteforce_dfs;
 mod bruteforce_bfs_flood;
+mod bruteforce_bfs_mark;
 
 pub static INFO: std::sync::LazyLock<Problem> = std::sync::LazyLock::new(||
     Problem::init(81, "Path Sum: Two Ways")
         .with_answer(427337)
         .solution("brute force, depth first search", bruteforce_dfs::solve)
         .solution("brute force, breadth first, flood", bruteforce_bfs_flood::solve)
+        .solution("brute force, breadth first, mark", bruteforce_bfs_mark::solve)
 );
 
 pub fn load() -> Vec<Vec<i64>> {
